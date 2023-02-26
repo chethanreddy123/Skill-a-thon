a3413a382c48380155da126848b7b0d93da1bef6
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-02-27 00:05:12 +0530

    added

--- MainRoute.py
+++ MainRoute.py
@@ -6,6 +6,48 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import ast
 
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from statistics import harmonic_mean
+from langdetect import detect
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, CountVectorizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import f1_score
+from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv('coursea_data.csv')
+df.drop(['Unnamed: 0', 'course_organization'], axis=1, inplace=True)
+df = df[df.course_students_enrolled.str.endswith('k')]
+
+df['course_students_enrolled'] = df['course_students_enrolled'].apply(lambda enrolled : eval(enrolled[:-1]) * 1000)
+
+minmax_scaler = MinMaxScaler()
+scaled_ratings = minmax_scaler.fit_transform(df[['course_rating','course_students_enrolled']])
+
+df['course_rating'] = scaled_ratings[:,0]
+df['course_students_enrolled'] = scaled_ratings[:,1]
+df['overall_rating'] = df[['course_rating','course_students_enrolled']].apply(lambda row : harmonic_mean(row), axis=1)
+
+minmax_scaler = MinMaxScaler()
+scaled_ratings = minmax_scaler.fit_transform(df[['course_rating','course_students_enrolled']])
+
+df = df[df.course_title.apply(lambda title : detect(title) == 'en')]
+
+
+vectorizer = TfidfVectorizer(stop_words='english')
+vectors = vectorizer.fit_transform(df.course_title)
+
+
+def recommend_by_course_title (title, recomm_count=10) : 
+    title_vector = vectorizer.transform([title])
+    cosine_sim = cosine_similarity(vectors, title_vector)
+    idx = np.argsort(np.array(cosine_sim[:,0]))[-recomm_count:]
+    sdf = df.iloc[idx].sort_values(by='overall_rating', ascending=False)
+    return sdf
+
+
+
+
 load_dotenv()
 
 
@@ -176,16 +218,30 @@ async def get_chat(info : Request):
 
 
 
+@app.post("/get_course_recommendation/")
+async def get_course_recommendation(info : Request):
+  print(await info.body())
+  infoDict = await info.json()
+  infoDict = dict(infoDict)
+
+  dk = recommend_by_course_title(infoDict['SearchString'])
+  List_Courses = dk.to_dict('records')
+
+  return {"List_Course" : List_Courses}
+
+
+
+@app.post("/get_job_recommendation/")
+async def get_job_recommendation(info : Request):
+  print(await info.body())
+  infoDict = await info.json()
+  infoDict = dict(infoDict)
+
+
+
+
+
+
 @app.get("/getInformation")
 def getInformation(info : Request):
   return FileResponse("obama.mp4")
-
-
-
-
-
-
-
-
-
-
